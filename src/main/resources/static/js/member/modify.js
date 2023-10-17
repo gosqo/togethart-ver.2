26f19c88c3228a9c6e5ee6f271a5a0c97ce03bd2
[songseongju] <[sj88139@naver.com]>
2023-10-17 10:42:44 +0900

    2차 파일커밋

--- src/main/resources/static/js/member/modify.js
+++ src/main/resources/static/js/member/modify.js
@@ -1,3 +1,31 @@
+// // 이미지 입력 필드와 프로필 이미지 엘리먼트 가져오기
+// const imageInput = document.getElementById('image-input');
+// const profileImg = document.getElementById('profile-img');
+
+// // 이미지 입력 필드에 변화가 생겼을 때 이벤트 처리하기
+// imageInput.addEventListener('change', function(event) {
+//     const file = event.target.files[0]; // 업로드된 파일 가져오기
+//     const reader = new FileReader(); // 파일 리더 생성
+
+//     // 파일 리더가 이미지를 읽으면 이벤트 처리하기
+//     reader.addEventListener('load', function() {
+//         profileImg.src = reader.result; // 프로필 이미지 엘리먼트에 이미지 소스 설정하기
+//         localStorage.setItem('profileImage', reader.result); // 이미지 데이터를 Local Storage에 저장하기
+//     });
+
+//     // 파일 리더로 이미지 읽기
+//     reader.readAsDataURL(file);
+// });
+
+// // 페이지 로드 시 Local Storage에서 프로필 이미지 가져오기
+// window.addEventListener('load', function() {
+//     const savedImage = localStorage.getItem('profileImage');
+
+//     if (savedImage) {
+//         profileImg.src = savedImage;
+//     }
+// });
+
 const form = document.querySelector('#form');
 form.addEventListener('submit', handleFormSubmit);
 
