ca7ad0bb747a71c3880ee6978f04101392a788eb
[songseongju] <[sj88139@naver.com]>
2023-10-31 14:24:31 +0900

    코드정리 최종본

--- src/main/java/com/team/togethart/controller/member/OAuthController.java
+++ src/main/java/com/team/togethart/controller/member/OAuthController.java
@@ -10,7 +10,6 @@ import com.team.togethart.dto.member.MemberAddRequest;
 import com.team.togethart.dto.member.OAuthToken;
 import com.team.togethart.repository.member.MemberMapper;
 import com.team.togethart.service.MemberService;
-import com.team.togethart.service.OAuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
@@ -33,7 +32,6 @@ import java.util.UUID;
 @AllArgsConstructor
 @RequestMapping("/auth")
 public class OAuthController {
-
     @Autowired
     private JwtUtils jwtUtils;
     @Autowired
@@ -42,45 +40,30 @@ public class OAuthController {
     private HttpServletResponse httpServletResponse;
 
     @Autowired
-    private OAuthService oAuthService;
-
-    @Autowired
     private MemberMapper memberMapper;
-
     @GetMapping("/kakao/callback")
     public ResponseEntity<?> kakaocallback(String code) throws JsonProcessingException, URISyntaxException {
-
         //post방식으로 key = value 데이터를 요청 (카카오쪽으로)
-
         RestTemplate rt = new RestTemplate();
-
         //http body 오브젝트 생성
-
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", "adbbf5ce440e188cfcb07e0628098935");
         params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
         params.add("code", code);
-
         //http Header와 HttpBody를 하나의 오브젝트에 담기
-
         HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
                 new HttpEntity<>(params, headers);
-
         // Http 요청하기 - Post 방식으로 - 그리고 response 변수의 응답 받음.
-
         ResponseEntity<String> response = rt.exchange(
                 "https://kauth.kakao.com/oauth/token",
                 HttpMethod.POST,
                 kakaoTokenRequest,
                 String.class
         );
-
         // DTO ObjectMapper 파싱
-
         ObjectMapper objectMapper = new ObjectMapper();
         OAuthToken oauthToken = null;
         try {
@@ -91,25 +74,17 @@ public class OAuthController {
             e.printStackTrace();
         }
         System.out.println("카카오 엑세스 토큰:" + oauthToken.getAccess_token());
-
-
         //  -------------------------------------------------------------------------------------------------//
 
         RestTemplate rt2 = new RestTemplate();
-
         //http body 오브젝트 생성
-
         HttpHeaders headers2 = new HttpHeaders();
         headers2.add("Authorization", "Bearer " + oauthToken.getAccess_token());
         headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
         //http Header와 HttpBody를 하나의 오브젝트에 담기.
-
         HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 =
                 new HttpEntity<>(headers2);
-
         // Http 요청하기 - Post 방식으로 - 그리고 response 변수의 응답 받음.
-
         ResponseEntity<String> response2 = rt2.exchange(
                 "https://kapi.kakao.com/v2/user/me",
                 HttpMethod.POST,
@@ -117,8 +92,8 @@ public class OAuthController {
                 String.class
         );
         System.out.println(response2.getBody());
-        // DTO ObjectMapper 파싱.
 
+        // DTO ObjectMapper 파싱.
         ObjectMapper objectMapper2 = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         KakaoProfile kakaoProfile = null;
@@ -129,7 +104,6 @@ public class OAuthController {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-
         // User 오브젝트 : username, password , email
         System.out.println("카카오 아이디 (번호): " + kakaoProfile.getId());
         System.out.println("카카오 이메일 : " + kakaoProfile.getKakao_account().getEmail());
@@ -144,7 +118,6 @@ public class OAuthController {
                 .memberEmail(kakaoProfile.getKakao_account().getEmail())
                 .build();
         // 가입자 확인 . 없을경우 자동 회원가입 DB에 저장 후 jwt 토큰 발급 안시켜주고 메인페이지 이동
-
         MemberAddRequest originUser = memberService.findMember(kakaoUser.getMemberUsername());
         if (originUser == null) {
             System.out.println("기존회원이 아니므로 자동으로 회원가입이 됩니다.");
@@ -153,10 +126,8 @@ public class OAuthController {
             HttpHeaders headers3 = new HttpHeaders();
             headers3.setLocation(URI.create("/"));
             return new ResponseEntity<>(headers3, HttpStatus.MOVED_PERMANENTLY);
-
         }
         // 위에 if null 값 으로 회원아닌 사람들 걸러 낸 후 기존 있던 회원 이므로 jwt 토큰 발급 후 자동 로그인
-
         String accessToken = jwtUtils.createAccessToken2("Togethart", originUser.getMemberId()
                 , kakaoProfile.getKakao_account().getEmail(), kakaoProfile.getKakao_account().getEmail() + "_" + kakaoProfile.getId());
         httpServletResponse.setHeader("Authorization", "Bearer " + accessToken);
@@ -170,22 +141,4 @@ public class OAuthController {
         headers4.setLocation(URI.create("/"));
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
-
-// ------------------------------------------- Google 소셜 로그인 --------------------------------------------------------------------------------------
-
-    @GetMapping("/google/callback")
-
-    public ResponseEntity<?> successGoogleLogin(@RequestParam("code") String accessCode) {
-
-        return oAuthService.getGoogleAccessToken(accessCode);
-
-    }
 }
-
-
-
-
-
-
-
-
